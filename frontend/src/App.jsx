108c29d82fadec97c83d3c04131fbae38172daec
R.M.P Harsha <prabhathharsha77@gmail.com>
2026-06-30 22:34:32 +0530

    feat: add route for StudentsDashbourd page in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import BrowseProjects from './pages/BrowseProjects';
 import AdminDashboard from './pages/AdminDashboard';
+import StudentsDashbourd from './pages/StudentsDashbourd';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/projects" element={<BrowseProjects/>} />
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/studentsdashbourd" element={<StudentsDashbourd/>} />
       </Routes>
     </Router>
   );
